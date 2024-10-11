d25cffef28b1ba7b81b2d2d959491049394e9873
bhschool24 <471752@cats.angletonisd.net>
2024-10-11 14:44:34 -0500

    update

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ document.addEventListener("DOMContentLoaded", function() {
             card.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
             card.style.opacity = 1;
             card.style.transform = 'translateY(0)';
-        }, 300);
+        }, 800);
     });
 });
